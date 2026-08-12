249437ebdd8764facfd38fab6ed7200a0d6e7ebd
Welington dos Santos Paiva <v-welingtonpaiva@cmoc.com>
2026-08-12 12:47:52 -0300

    chore(models): regenera codigo dos models Isar (.g.dart)

--- lib/features/report_form/data/models/report_model.g.dart
+++ lib/features/report_form/data/models/report_model.g.dart
@@ -15,7 +15,7 @@ extension GetReportModelCollection on Isar {
 
 const ReportModelSchema = CollectionSchema(
   name: r'ReportModel',
-  id: 3139160268685868681,
+  id: 3139160268685868544,
   properties: {
     r'availableMaterials': PropertySchema(
       id: 0,
@@ -85,7 +85,7 @@ const ReportModelSchema = CollectionSchema(
   idName: r'id',
   indexes: {
     r'uuid': IndexSchema(
-      id: 2134397340427724972,
+      id: 2134397340427725056,
       name: r'uuid',
       unique: true,
       replace: true,
@@ -2605,7 +2605,7 @@ extension ReportModelQueryProperty
 
 const EmbeddedCollaboratorModelSchema = Schema(
   name: r'EmbeddedCollaboratorModel',
-  id: 5824696152780669677,
+  id: 5824696152780669952,
   properties: {
     r'id': PropertySchema(id: 0, name: r'id', type: IsarType.string),
     r'isCustom': PropertySchema(id: 1, name: r'isCustom', type: IsarType.bool),
